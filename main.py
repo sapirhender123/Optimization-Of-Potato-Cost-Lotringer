8fedcccc59b596d5961adecbeafa99d14d1d9be1
Sapir Hender <sapirhender@gmail.com>
2022-02-10 18:25:12 +0200

    Add comments + remove loops array

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from numpy import random
 import random as rand
 
-
+decreasingPrice = 10
+# The time have an exponential distribution, and as long as the simulation is running it increase
 def getTime(timeStamp):
     x = random.exponential(scale=1, size=(1, 1)) + timeStamp
     return x
 
-
+# Three options for prices, the last one is price that decreasing according to the time
 def getPrice(num):
     if num == 1:
         return 5
     elif num == 2:
-        return 50
+        return 8
     elif num == 3:
-        return 100
+        global decreasingPrice
+        decreasingPrice = 0.99 * decreasingPrice
+        return decreasingPrice
 
-
+# Calculate the probability of customer to purchase
 def purchaseProb(option, timeStamp):
     timeStamp = getTime(timeStamp)
     return 1/timeStamp + 1/getPrice(option), timeStamp
 
-
+# Uniform value that represent a threshold which above it customer is purchasing
 def purchaseThreshold():
     randNum = rand.uniform(0.0, 1.0)
     return randNum
@@ -28,27 +31,28 @@ def purchaseThreshold():
 
 def main():
     times = []
-    loops = []
+    # Iterating the prices
     for index in range(1,4):
         sumTime = 0
-        sumLoops = 0
+        # Calculating the average from 15 loops
         for i in range(15):
             inventory = 100
             timeStamp = 0
+            global decreasingPrice
+            decreasingPrice = 10
+            # Simulating the purchasing until inventory is over
             while (inventory > 0):
-                # buying
                 purchProb, timeStamp = purchaseProb(index, timeStamp)
                 if (purchProb > purchaseThreshold()):
-                    amount = rand.uniform(1.0, 5.0)
+                    # purchasing
+                    amount = rand.uniform(1.0, 5.0) # Random amount of inventory
                     inventory = inventory - amount
-                    sumLoops += 1
             sumTime = sumTime + timeStamp
+        # Calculating the average of all the loops
         avgTime = sumTime / 15
-        avgLoops = sumLoops / 15
         times.append(avgTime)
-        loops.append(avgLoops)
-        print("The average time of index " ,index, " is " ,times[index - 1])
-        print("The average number of loops until the inventory ends in index ", index, " is ",loops[index - 1])
+        print("The average time of index ",index," is ",times[index - 1])
+
 
 if __name__ == '__main__':
     main()
